85cf589f461fce1c7649cc0b8a6281057284d4ae
Jathin Chetty <jathin.12221556@lpu.in>
2022-12-02 00:02:18 +0530

    initial commit

---
+++ class2-Primitive Data Types, Comments, Variables-CipherSchools.py
@@ -0,0 +1,49 @@
+#Printing Hello World#
+print("Hello")
+print("Hello World")
+
+#omments
+#This is a comment
+
+'''
+Every line executed in python is either
+
+ 1)Statement
+ 2)Expression
+'''
+
+"""
+My Name Is jathin
+"""
+
+#Data Types
+print(isinstance("a",str))
+print(isinstance("a",int))
+print(isinstance(2,str))
+print(isinstance("a",object))
+print(isinstance(2,object))
+print(isinstance(print,object))
+
+a=255
+print(a)
+a="a"
+print(a)
+type(a)
+print(id(a))
+b=200
+print(id(b))
+c=278
+print(id(c))
+
+a="aaaaa"
+print(id(a))
+b="aaaaa"
+print(id(b))
+
+a=5639475024705025027420348845345
+print(a+1)
+
+#Variables
+_a=12
+a1=1
+
---
+++ class5-Operators and Expressions-CipherSchools.py
@@ -0,0 +1,30 @@
+#Arithmetic Operators
+print(5+5)
+print(10-5)
+print(10/3) #Dividing int by int gives int in python2, but not in python 3
+print(isinstance(10.0,int))
+print(10//3) #Floor Division
+print(10%3)
+print(2*3)
+print(2**3) #Exponentiation
+print("jatin"+"katyal")
+print("abc"*6)
+print("jathin" "" "chetty") #String Concatenation
+
+#Comparision Operators
+print(1==2)
+print(1!=2)
+print(1<2)
+print(2>3)
+print("ab"<"z") #Lexicographical comparison -> Follows dictionary order
+print("a"<"A") #Dunders
+
+#Logical Operators
+print(True and False)
+print(True or False)
+print(True and 1)
+print(1 and 0)
+print(1 and 5)
+print(isinstance(True,int)) #Returns True--->True is integer
+print(type(True))
+print(bool(1))
